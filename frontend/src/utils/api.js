ef74c63fc4893e44182486bc4b083f6e81c4c99e
AnoopGutti7 <gutiianoop1@gmail.com>
2026-06-13 16:33:44 +0530

    Debug API base

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -7,6 +7,8 @@ import { getAuthToken } from "./authStorage";
 export const API_BASE =
   import.meta.env.VITE_API_BASE ||
   "/api";
+  
+  console.log("API_BASE =", API_BASE);
 
 /**
  * Axios instance
